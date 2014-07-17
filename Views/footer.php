abeb4ec257c55183fd46769e22fb5db17a5f094f
g-eleutheriou <giwrgos.eleutheriou@gmail.com>
2014-07-18 02:09:10 +0300

    recommit previeus

--- Views/footer.php
+++ Views/footer.php
@@ -1,29 +1,22 @@
 </div>
+<div id="foot">
+	<div id="social">
+		<a id="facebook" href="https://www.facebook.com/"> <span class="icon"></span>
+			<span class="title">Facebook</span>
+		</a> <a id="twitter" href="https://twitter.com/"> <span class="icon"></span>
+			<span class="title">Twitter</span>
+		</a>
+	</div>
+	<div id="links">
+		<ul>
+			<li><a href="index.html">links</a></li>
+			<li><a href="#">map</a></li>
+			<li><a href="#">rules</a></li>
+			<li><a>EAM &copy; 2014</a></li>
 
-		<footer class="footer container">
-			<hr>
-			<p>Copyright &copy; 2014 EAM &reg;</p>
-		</footer>
-
-
-<!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-<!--     <script src="<?php echo BOOTSTRAP; ?>/js/jquery.js"></script>
-     <script src="<?php echo BOOTSTRAP; ?>/js/jquery.tablesorter.min.js"></script> 
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap.min.js"></script>      
-	<script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-transition.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-alert.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-modal.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-dropdown.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-scrollspy.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-tab.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-tooltip.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-popover.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-button.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-collapse.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-carousel.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap-typeahead.js"></script>
-    <script src="<?php echo BOOTSTRAP; ?>/js/bootstrap.js"></script>-->
+		</ul>
+	</div>
+</div>
+</div>
 </body>
 </html>
--- Views/header.php
+++ Views/header.php
@@ -1,29 +1,50 @@
 <!doctype html>
 <html>
 <head>
-  <meta charset="utf-8">
-	<title>Eudoxus</title>
-    
-    <!-- metadata -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="<?php echo STYLES; ?>/styles.css" rel="stylesheet">	
-    
-</head>
-<body>  
-	
-	<div id="navbar">
-		<nav>
-			<ul>
-				<li><img src="<?php echo STYLES; ?>/images/home.png"><a href="home.html">Home</a></li>
-				<li><a href="about.html">About Us</a></li>
-				<li><a href="help.html">Contact</a></li>
-			</ul>
-		</nav>
-	</div>
-	
-	
-	<div id="main" class="container">
+<meta charset="utf-8">
+<title>Eudoxus</title>
 
- 
+<!-- metadata -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<link href="<?php echo STYLES; ?>/footer.css" rel="stylesheet">
+<link href="<?php echo STYLES; ?>/header.css" rel="stylesheet">
+<link href="<?php echo STYLES; ?>/index.css" rel="stylesheet">
+
+</head>
+<body>
+	<div id="main">
+		<div id="head">
+			<a id="logo" href="index.html">logo</a>
+			<div id="navmenu">
+
+				<nav>
+					<ul>
+						<li id="home"><a href="index.html"> <span class="icon"></span> <span
+								class="title">Home</span>
+						</a></li>
+						<li id="about"><a href="#"> <span class="icon"></span> <span
+								class="title">About</span>
+						</a></li>
+						<li id="help"><a href="#"> <span class="icon"></span> <span
+								class="title">Help</span>
+						</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div id="head-right">
+				<div id="search">
+					<form action="">
+						<input placeholder="Search here" type="text" />
+					</form>
+				</div>
+				<div id="user-btn">
+					<p>
+						<button id="login">
+							<span class="icon"></span> <span class="text">Login</span>
+						</button>
+					</p>
+				</div>
+			</div>
+		</div>
