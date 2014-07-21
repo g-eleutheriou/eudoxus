d7b77fc62b75e2a4ef90215c428c7f13cd494d27
g-eleutheriou <giwrgos.eleutheriou@gmail.com>
2014-07-21 08:14:37 +0300

    test

--- Views/page/about.php
+++ Views/page/about.php
@@ -2,26 +2,14 @@
 	<div id="top">
 		<!-- based on http://thecodeplayer.com/walkthrough/css3-breadcrumb-navigation-->
 		<div class="breadcrumb">
-			<a href="<?php echo HOME; ?>">Home</a>
-			<a href="#" class="active">About</a>
+			<a href="<?php echo HOME; ?>">Αρχίκη</a>
+			<a href="#" class="active">Σχετίκα με τον Εύδοξο</a>
 		</div>
 	</div>
-	<aside id="aside">
-		<p>
-			illustration
-		</p>
-	</aside>
-	<section id="article">
-		<article>
-			<header>
-				<h2>Title</h2>
-				<p>
-					content
-				</p>
-			</header>
-			<p>
-				content
-			</p>
-		</article>
-	</section>
+	<div id="aboutWrapper">
+		<div id="aboutContainer">
+			
+		</div>
+	</div>
 </div>
+
