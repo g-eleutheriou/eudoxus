a4c49c3ab3149b419b79f14dde52c52aded86c8e
g-eleutheriou <giwrgos.eleutheriou@gmail.com>
2014-07-21 09:07:35 +0300

    test

--- Views/page/searchResults.php
+++ Views/page/searchResults.php
@@ -51,9 +51,9 @@
 					echo "</table>";
 					echo "</div>";
 					echo "<div class=\"actions\">";
-					echo "<form id=\"Addbtn1\" onsubmit=\"return check()\" method=\"get\" action=\"\"><input type=\"submit\" value=\"Δήλωση\" /></form>";
-					echo "<form id=\"Addbtn2\" onsubmit=\"return check()\" method=\"get\" action=\"\">";
-					echo "<input type=\"submit\" value=\"Ανταλλαγή\" />";
+					echo "<form id=\"Addbtn1\" ><input type=\"button\" value=\"Δήλωση\" /></form>";
+					echo "<form id=\"Addbtn2\" >";
+					echo "<input type=\"button\" value=\"Ανταλλαγή\" />";
 					echo "</form>";
 					echo "</div>";
 					echo "</section></td>";
